6e4131881cb25758a4a60fa341d58351c4aa20b8
Vivek Gidmare <vivekgidmare@gmail.com>
2018-12-29 18:02:13 +0530

    Initial commit

---
+++ app/src/main/java/sample/SampleAndroid.kt
@@ -0,0 +1,27 @@
+package sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+actual class Sample {
+    actual fun checkMe() = 44
+}
+
+
+actual object Platform {
+    actual val name: String = "Android"
+}
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val helloMessage = hello()
+        val checked = Sample().checkMe()
+        setContentView(R.layout.activity_main)
+        Log.d("TAG", "$checked::$helloMessage")
+        tv_hello.text = helloMessage
+    }
+}
