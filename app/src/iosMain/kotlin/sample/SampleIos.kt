81e3a02000be7d7dead79d70644caa392b45c78a
Vivek Gidmare <vivekgidmare@gmail.com>
2019-03-19 23:15:16 +0530

    added first change for iOS

--- app/src/iosMain/kotlin/sample/SampleIos.kt
+++ app/src/iosMain/kotlin/sample/SampleIos.kt
@@ -1,9 +1,13 @@
 package sample
+import platform.UIKit.UIDevice
 
 actual class Sample {
     actual fun checkMe() = 7
 }
 
 actual object Platform {
+//    UIDevice.currentDevice.systemName() +
+//    " " +
+//    UIDevice.currentDevice.systemVersion
     actual val name: String = "iOS"
 }
--- app/src/main/java/sample/SampleAndroid.kt
+++ app/src/main/java/sample/SampleAndroid.kt
@@ -18,9 +18,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         val helloMessage = hello()
         val checked = Sample().checkMe()
-        setContentView(R.layout.activity_main)
         Log.d("TAG", "$checked::$helloMessage")
         tv_hello.text = helloMessage
     }
